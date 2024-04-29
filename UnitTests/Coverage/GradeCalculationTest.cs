1185acc3b4ab9293247e4ad76566e45822f476dc
Rodrigo Sedano <r_sedano@hotmail.com>
2024-04-29 22:33:03 +0200

    Add unit test

--- UnitTests/Coverage/GradeCalculationTest.cs
+++ UnitTests/Coverage/GradeCalculationTest.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+namespace UnitTests.Coverage
+{
+	public class GradeCalculationTest
+	{
+		[Fact]
+		public void GetGradeA()
+		{
+			//Arrange
+			int mark = 90;
+			string expectedGrade = "A";
+
+			//Act
+			var grade = GradeCalculation.getGrade(mark);
+
+			//Assert
+			Assert.Equal(expectedGrade, grade);
+		}
+	}
+}
